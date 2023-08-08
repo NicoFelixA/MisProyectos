46f7cc80764bca02b564c9303a0442747300f129
NicoFelixA <124410869+NicoFelixA@users.noreply.github.com>
2023-08-07 17:32:42 -0700

    Add files via upload

---
+++ index/propiedad.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="es" data-bs-theme="dark">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Logra || Bienes y raices</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+  <link rel="stylesheet" href="LograCSS.css">
+  
+</head>
+<body class="grid-container">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+    <a href="https://api.whatsapp.com/send?phone=526672230819&text=Hola%20buenas%20tardes.%20Me%20gustaria%20informes%20sobre%20una%20propiedad." class="float" target="_blank">
+    <i class="fa fa-whatsapp my-float"></i>
+    </a>
+  <header>
+  <div class="lg-fixed-menu lg-fixed-menu--transparent-light">
+    <div class="lg-top-header lgx-top-header--primary">
+      <div class="lg-layout">
+      <span class="lg-top-header__element lg-top-header__element--phone">
+        <a href="tel:6672230819">
+        <i class="flaticon-phone">667 223 0819</a></i>
+      </span>
+      <address>
+        <i class="flaticon-pin"> Culiacán, Sinaloa</i>
+     
+      </address>
+    </span>
+      </div>
+    </div>
+  </div>
+  <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+          <h1 class="navbar-brand" href="#">LOGRA <br> Bienes Y Raices</h1>
+          <button class="navbar-toggler" stype="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li>
+                <img src="Logra.jpg" alt="" class="logo" >
+              </li>
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#Casas">Casas</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="https://goo.gl/maps/u3yW8SZPRzDKUWDd7">Ubicaciones</a>
+              </li>
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Mas opciones
+                </a>
+                <ul class="dropdown-menu">
+                  <li><a class="dropdown-item" href="tel: +526674518294">Contactar</a></li>
+                  <li><a class="dropdown-item" href="#contacto">Dudas</a></li>
+                  <li><hr class="dropdown-divider"></li>
+                  <li><a class="dropdown-item" href="#">Mas informacion</a></li>
+                </ul>
+              </li>
+            </ul>
+            <form class="d-flex" role="search">
+              <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" >
+              <button type="button" class="btn btn-outline-light">Buscar</button>
+            </form>
+          </div>
+        </div>
+      </nav>
+     </header>
